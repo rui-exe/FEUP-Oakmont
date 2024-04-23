7ac33b07f7850c5f6201204c645e19a387526b63
rui-exe <mrswager173@gmail.com>
2024-04-23 10:58:36 +0100

    feat: authenticated user can now create a post

--- backend/app/api/routes/financial_instruments.py
+++ backend/app/api/routes/financial_instruments.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter,Query,Path
-from app.api.deps import HBase
+from fastapi import APIRouter,Query,Path, HTTPException
+from app.api.deps import HBase,CurrentUser
 from app.crud import financial_instruments as financial_instruments_crud,posts as crud_posts
 from app.models.financial_instruments import FinancialInstrument,Tick
 from datetime import datetime,timedelta
@@ -7,7 +7,10 @@ from datetime import datetime,timedelta
 router = APIRouter()
 
 @router.get("/")
-async def get_financial_intsruments(db:HBase) -> list[FinancialInstrument]:
+async def get_financial_instruments(db:HBase) -> list[FinancialInstrument]:
+  """
+  Get all financial instruments
+  """
   return financial_instruments_crud.get_symbols(db)
 
 @router.get("/{symbol}")
@@ -21,8 +24,25 @@ async def get_financial_instruments_with_params(
     return financial_instruments_crud.get_instrument_prices(db, symbol, start_date, end_date, interval)
 
 @router.get("/{symbol}/posts")
-def get_user_posts(db:HBase, symbol:str):
+def get_symbol_posts(db:HBase, symbol:str = Path(..., description="Symbol of the financial instrument")):
   """
-  Get the posts of a user.
+  Get the posts of a symbol
   """
   return crud_posts.get_symbol_posts(db, symbol)
+
+@router.post("/{symbol}/posts")
+def create_new_post(db: HBase, current_user: CurrentUser, symbol: str = Path(..., description="Symbol of the financial instrument"), text: str = Query(..., description="Text of the post")):
+    """
+    Create a new post for a symbol.
+    """
+    if not symbol:
+        raise HTTPException(status_code=400, detail="Symbol is required")
+    if not text:
+        raise HTTPException(status_code=400, detail="Text is required")
+    
+    post = crud_posts.create_new_post(db, {
+        "username": current_user.username,
+        "symbol": symbol,
+        "text": text
+    })
+    return post
--- backend/app/api/routes/posts.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter,Query,Path
-from app.api.deps import HBase
-from app.crud import posts as posts_crud
-from app.models.posts import Post
-from datetime import datetime
-
-router = APIRouter()
-
-@router.get("/{posts}")
